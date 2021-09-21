a7a4ec2b452f13d448cb3c79cd3f1cfdd8184005
Filipe Ferreiro Pereira <80466386+filipeferreiro@users.noreply.github.com>
2021-09-21 18:37:10 -0300

    localização

--- app/src/main/java/com/example/animeapp/MainActivity.java
+++ app/src/main/java/com/example/animeapp/MainActivity.java
@@ -6,14 +6,19 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Service;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -34,12 +39,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
     private SensorEventListener sensorEventListenerLight;
+    int REQUEST_LOCATION = 88;
     /* Variaveis para a mudança de cor com sensor */
     SensorManager sensorManager;
     Sensor sensor;
@@ -99,7 +118,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             public void onSensorChanged(SensorEvent event) {
                 float floatSensorValue = event.values[0]; // Lux
 
-                if(floatSensorValue < 35){
+                if(floatSensorValue < 45){
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                     SharedPreferences.Editor editor = sharedPreferences.edit();
                     editor.putBoolean("night_mode",true);
@@ -118,6 +137,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         };
 
+        getLocation();
     }
 
     @Override
@@ -144,11 +164,74 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 intent.putExtra(Intent.EXTRA_SUBJECT,"Sharing URL");
                 intent.putExtra(Intent.EXTRA_TEXT, "Ainda não temos um website, mas você pode ver o site da nossa escola :) www.basilides.com.br");
                 startActivity(Intent.createChooser(intent, "Share URL"));
+                break;
 
+            case R.id.nav_loc:
+                if(ActivityCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+                    Location();
+                }else{
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},23);
+                }
         }
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void getLocation() {
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+        };
+
+        TedPermission.create()
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
+    }
+
+    private void Location() {
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setInterval(5000);
+        request.setFastestInterval(2000);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(request);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+        result.addOnCompleteListener(task -> {
+            try{
+                LocationSettingsResponse response = task.getResult(ApiException.class);
+                // FAZER A TAREFA COM A LOCALIZAÇÃO AQUI
+            } catch (ApiException e) {
+
+                switch (e.getStatusCode()){
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try{
+                            ResolvableApiException resolvableApiException = (ResolvableApiException)e;
+                            resolvableApiException.startResolutionForResult(MainActivity.this,REQUEST_LOCATION);
+                        } catch (IntentSender.SendIntentException sendIntentException){
+                        }
+                        break;
+                        // QUANDO O DISPOSITIVO NÂO TIVER LOCALIZAÇÃO
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        break;
+                }
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/animeapp/RegistroActivity.java
+++ app/src/main/java/com/example/animeapp/RegistroActivity.java
@@ -1,11 +1,15 @@
 package com.example.animeapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toolbar;
 
 public class RegistroActivity extends AppCompatActivity {
@@ -27,6 +31,7 @@ public class RegistroActivity extends AppCompatActivity {
                 finish();
             }
         });
-    }
 
+
+    }
 }
